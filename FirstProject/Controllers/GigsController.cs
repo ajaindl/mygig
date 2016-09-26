90d220f3e9143bce6e561ebd1e82cd0accbf6b10
ajaindl <ajaindl@gmail.com>
2016-09-26 12:27:50 -0400

    add a gig form

--- FirstProject/Controllers/GigsController.cs
+++ FirstProject/Controllers/GigsController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor;
+using FirstProject.Models;
+using FirstProject.ViewModels;
 
 namespace FirstProject.Controllers
 {
     public class GigsController : Controller
     {
-        // GET: Gigs
-        public ActionResult Createc()
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
         {
-            return View();
+            _context=new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var viewModel=new GigFormViewModel { 
+                Genres= _context.Genres.ToList()
+                };
+            return View(viewModel);
         }
     }
 }
